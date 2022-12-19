c1852e01bbafddd09703926ba75914c6f2de6b04
«Oksana <oxana.shishkova@gmail.com>
2022-12-19 15:03:40 +0300

    Start committing

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// №6 На вход принимаем число, на выходе выдаем, является ли число четным
+
+
+Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number %2 ==0)
+    Console.WriteLine("Even number");
+else
+    Console.WriteLine("Odd number");
